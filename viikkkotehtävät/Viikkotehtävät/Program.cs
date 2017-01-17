1185e7b9fbd45c631222daadc12a2e2a08296a66
Petri <K1735@student.jamk.fi>
2017-01-17 18:42:10 +0200

    teht 2 donezo

--- viikkkotehtävät/Viikkotehtävät/Program.cs
+++ viikkkotehtävät/Viikkotehtävät/Program.cs
@@ -11,7 +11,7 @@ namespace Viikkotehtävät
 
         static void Main()
         {
-            Console.WriteLine("Tereve tulemast, paina 1");
+            Console.WriteLine("Tereve tulemast, paina 1 tai 2");
             string retval = Console.ReadLine();
             int valinta = int.Parse(retval);
 
@@ -20,9 +20,10 @@ namespace Viikkotehtävät
                 case 1:
                     tehtava1();
                     break;
-                    //case 2:
-                    //tehtava2();
-                    // break;
+
+                case 2:
+                    tehtava2();
+                    break;
 
 
             }
@@ -56,10 +57,51 @@ namespace Viikkotehtävät
             }
         } //teht 1 lopppuu
 
+        static void tehtava2()
+        {
+            Console.WriteLine("Paljonkas sait poika pisteitä kokeesta?");
+            string retval = Console.ReadLine(); // Retval palauttaa arvon 'return value'
+            int numero = int.Parse(retval);
+
+            if (numero == 0 || numero == 1)
+            {
+                retval = "numerosi on silti 0 LUUSERI";
+                Console.WriteLine(retval);
+            }
+
+            else if (numero == 2 || numero == 3)
+            {
+                retval = "numerosi on silti vain 1 LUUSERI";
+                Console.WriteLine(retval);
+            }
+
+            else if (numero == 4 || numero ==5)
+            {
+                retval = "numerosi on silti vain 2 LUUSERI";
+                Console.WriteLine(retval);
+            }
+            else if (numero == 6 || numero == 7)
+            {
+                retval = "numerosi on silti vain 3 LUUSERI";
+                Console.WriteLine(retval);
+            }
+            else if (numero == 8 || numero == 9)
+            {
+                retval = "numerosi alkaa olla jo hyvä mutta silti vain 4 LUUSERI";
+                Console.WriteLine(retval);
+            }
+            else if (numero == 10 || numero == 11 || numero == 12)
+            {
+                retval = "OHO poikahan pisti. numero: 5";
+                Console.WriteLine(retval);
+            }
 
 
 
 
 
-    } //tän yläpuolelle tehtäviä
+
+
+        } //tän yläpuolelle tehtäviä
+    }
 }
