6859245780068966ec4d49d9b58a2163d12c860f
Petri <K1735@student.jamk.fi>
2017-01-18 17:12:34 +0200

    Did excercise 3

---
+++ Viikkotehtavat/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viikkotehtavat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Katteleppasiitä kotitehtäviä");
+            Tehtava1();
+        }
+        static void Tehtava1()
+        {
+            //Tee ohjelma, joka tulostaa käyttäjän antamaa lukua (1, 2 tai 3) 
+            //vastaavan luvun sanana (yksi, kaksi tai kolme). Jos käyttäjä syöttää 
+            //jonkin muun luvun, tulee näytölle tulostaa teksti: "jokumuu luku".
+            Console.WriteLine("Anna 1, 2 tai 3 ja tulostan sen tekstinä");
+            string retval = Console.ReadLine();
+                     //retval --> return value!
+            int luku = int.Parse(retval);
+            //parse = voi stringejä ''parsia'' luvuiksi.
+            if (luku == 1)
+            {
+                retval = "kirjoitit luvun yksi";
+                // tulostus käyttäjälle
+                Console.WriteLine(retval);
+            }
+            else if (luku == 2) ;
+            {
+                retval = "kirjoitit luvun kaksi";
+                Console.WriteLine(retval);
+         
+            }
+            else if (luku == 3) ;
+            {
+                retval = "kirjoitit luvun kolme";
+                Console.WriteLine(retval);
+            }
+
+
+        }
+    }
+}
--- Viikkotehtävät/Viikkotehtävät/Program.cs
+++ Viikkotehtävät/Viikkotehtävät/Program.cs
@@ -11,10 +11,16 @@ namespace Viikkotehtävät
 
         static void Main()
         {
-            Console.WriteLine("Tereve tulemast, paina 1 tai 2");
+            Valikko1();
+         }
+
+        static void Valikko1()
+        {
+            Console.WriteLine("Tereve tulemast tehtävien pariin");
+            Console.WriteLine("Paina joku numero 1-3(20): ");
             string retval = Console.ReadLine();
             int valinta = int.Parse(retval);
-
+            Console.Clear();
             switch (valinta)
             {
                 case 1:
@@ -25,15 +31,18 @@ namespace Viikkotehtävät
                     tehtava2();
                     break;
 
+                case 3:
+                    tehtava3();
+                    break;
 
             }
         }
-        static void tehtava1()
+            static void tehtava1()
         {
             Console.WriteLine("Kirjota luku 1,2 tai 3");
             string retval = Console.ReadLine(); // Retval palauttaa arvon 'return value'
             int luku = int.Parse(retval);
-
+            
             if (luku == 1)
             {
                 retval = "yksi";
@@ -54,7 +63,12 @@ namespace Viikkotehtävät
             {
                 retval = "blah";
                 Console.WriteLine(retval);
+
+               
             }
+            
+            Valikko1();
+            
         } //teht 1 lopppuu
 
         static void tehtava2()
@@ -95,13 +109,51 @@ namespace Viikkotehtävät
                 retval = "OHO poikahan pisti. numero: 5";
                 Console.WriteLine(retval);
             }
+            Valikko1();
 
 
 
 
 
 
+        }
 
-        } //tän yläpuolelle tehtäviä 1231231231231233122
-    }
+        static void tehtava3()
+        {
+           
+            ////Tee ohjelma, joka kysyy käyttäjältä kolme lukua ja tulostaa niiden summan ja keskiarvon.
+
+            
+
+            int[] array = new int[3];
+            Console.WriteLine("Kerro mulle kolme lukua niin tulostan niiden summan ja keskiarvon:");
+            for (int i = 0; i < 3; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int pituus = array.Length;
+            int summa = 0;
+            int keskiarvo = 0;
+
+        for (int i = 0; i < pituus; i++)
+            {
+
+            summa += array[i];
+
+            }
+
+                keskiarvo = summa / pituus;
+            Console.WriteLine("summa: " + summa);
+            Console.WriteLine("Keskiarvo: " + keskiarvo);
+            Console.ReadLine();
+
+
+
+
+
+            Valikko1();
+        }
+         
+
+    }//tän yläpuolelle tehtäviä 1231231231231233122
 }
