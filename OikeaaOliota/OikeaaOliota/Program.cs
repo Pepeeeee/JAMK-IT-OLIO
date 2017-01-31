2df7efc329ddaf7939c628151cfbedc6be149d46
Petri Penttinen <K1735@student.jamk.fi>
2017-01-31 17:02:02 +0200

    Auto

---
+++ OikeaaOliota/OikeaaOliota/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace Random  
+
+{
+
+     public class Student
+
+    {
+        // Properties:
+        public int Age { get; set; }
+        public string ID { get; set; }
+        public string Name { get; set; }
+        // Methods:
+        public void Eat()
+        {
+            Console.WriteLine("Omnom... ");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine("Krooh... ");
+        }
+        public void WriteCode()
+        {
+            Console.WriteLine("No voihan nyt vihvilä sentään.");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Luodaan opiskelijat:
+            Student jeppe = new Student();
+            jeppe.Age = 25;
+            jeppe.Name = "Jeppe";
+            jeppe.ID = "K1234";
+            Student junnu = new Student();
+            junnu.Age = 19;
+            junnu.Name = "Junnu";
+            junnu.ID = "K9934";
+            Student kete = new Student();
+            kete.Age = 21;
+            kete.Name = "Kete";
+            kete.ID = "K2222";
+            Student lennu = new Student();
+            lennu.Age = 24;
+            lennu.Name = "Lennu";
+            lennu.ID = "K4321";    
+            Student heka = new Student();
+            heka.Age = 30;
+            heka.Name = "Heka";
+            heka.ID = "K1334";
+            // Lisätään ne taulukkoon:
+            string[,] students = new string[5, 3]
+            {
+                { jeppe.Name, jeppe.ID, jeppe.Age.ToString()},
+                { junnu.Name, junnu.ID, junnu.Age.ToString() },
+                { kete.Name, kete.ID, kete.Age.ToString() },      
+                { lennu.Name, lennu.ID, lennu.Age.ToString() },
+                { heka.Name, heka.ID, heka.Age.ToString() }
+            };
+            // Tulostetaan teulukon paikat:
+            for (int i = 0; i < 5; i++
+            {
+                Console.WriteLine("Opiskelija {0}: ", (i + 1));
+                for (int j = 0; j < 3; j++)
+               {
+                    Console.WriteLine(students[i, j]);
+                }
+                Console.WriteLine();
+
+            }
+
+        }
+
+    }
+
+    
+
+    //^ esmerkki
+
---
+++ Viikkotehtävät/TTV16S4/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTV16S4
+{
+    class Car
+    { //property eli ominaisuus --> Auton nopeus.  Ominaisuudet ovat aina julkisia (public ....)
+        //näinkin voi tehdä mutta vältä
+        // public int Speed; <--- vältä
+
+        public int Horsepower { get; set; } 
+        //sisäinen muuttuja määrittelee maksiminopeuden joka oliolle voidaan asettaa.
+        private const int maxSpeed = 200; //const tarkoittaa vakiota (luku ei muutu)
+        private int speed;
+        public int Speed
+      
+        {
+            get //get - accessor palauttaa propertylle arvon
+            {
+                return speed;
+            }
+            //set accessorilla asetetaan propertyn arvoa
+            set
+            {
+                if (value > maxSpeed)
+                    speed = maxSpeed;
+                else
+                speed = value;
+            }   
+        }
+        //constructors
+        public Car()
+        { }
+        public Car (int horsepower, int speed)
+        {
+            Horsepower = horsepower;
+            if (speed < maxSpeed)
+                this.Speed = speed; //this. viittaa olioon itseensä
+            else
+                this.speed = maxSpeed;
+           
+        }
+
+
+        //methods
+        public void Accelerate(int additions)
+        {
+            //kiihdytetään autoa, muttei yli maksiminopeuden
+            if (speed + additions < maxSpeed)
+                speed = speed + additions;
+            else
+                speed = maxSpeed;
+        }
+    }
+    
+   
+   
+    class Program
+    {
+        static void TestMyCarClass()
+        {
+            //luodaan Car-olio, auto = uusi auto jolla nopeus 175.
+            Car car = new Car();
+            car.Speed = 175;
+            //luodaan toinen pirssi
+            Car car2 = new Car();
+            car2.Horsepower = 300;
+            car2.Speed = 250;
+            //luodaan kolmas pikkufiiu
+            Car fiat = new Car(55, 105);
+
+            //Näytetään konsolilla
+            Console.WriteLine("Autosi 1 nopeus on {0}", car.Speed);
+            Console.WriteLine("Autosi 2 Nopeus on {0}", car2.Speed);
+            //kiihdytetään kumpaakin autoa
+            car.Accelerate(20);
+            car2.Accelerate(20);
+            fiat.Accelerate(20);
+            Console.WriteLine("kiihdytettiin  20km/h");
+            Console.WriteLine("Autosi 1 nopeus on {0}", car.Speed);
+            Console.WriteLine("Autosi 2 Nopeus on {0}", car2.Speed);
+            Console.WriteLine("Autosi fiat Nopeus on {0}", fiat.Speed);
+
+        }
+        static void Main(string[] args)
+        {
+            TestMyCarClass();
+        }
+    }
+}
