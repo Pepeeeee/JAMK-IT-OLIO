63d914786f02ccc5503015630340fcf757fc91c7
Petri Penttinen <K1735@student.jamk.fi>
2017-03-07 17:19:12 +0200

    321

---
+++ Henkilörekisteri/L8T2 Ostokset/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L8T2_Ostokset
+{
+    class tuote
+    {
+        public string nimi { get; set; }
+        public double hinta { get; set; }
+        
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
--- Henkilörekisteri/NOPPA/Program.cs
+++ Henkilörekisteri/NOPPA/Program.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace NOPPA
+namespace noppa
 {
     class Program
     {
+        public static int Heitto()
+        {
+            Random r = new Random();
+            Thread.Sleep(32);
+            return r.Next(1, 7);
+        }
+
         static void Main(string[] args)
         {
+            List<int> randomm = new List<int>();
+            Console.Write("Monta ja mitä\n");
+            int lenght = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.WriteLine(Heitto());
+                randomm.Add(Heitto());
+            }
+            Console.WriteLine("\n\nDice was rolled {0} times and the average was {1}" , lenght , randomm.Average());
+
         }
+
+        
     }
 }
