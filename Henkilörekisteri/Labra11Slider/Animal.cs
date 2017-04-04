f6d855d1f90ce0715361b76c6feee61616ce1ff2
Petri Penttinen <K1735@student.jamk.fi>
2017-04-04 17:00:37 +0300

    PUSH

---
+++ Henkilörekisteri/Labra11Slider/Animal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra11Slider
+{
+    class Animal : INotifyPropertyChanged
+    {
+        private int happiness;
+        public int Happiness
+        {
+            get { return happiness; }
+            set
+            {
+                if (value != happiness)
+                {
+                    happiness = value;
+                    //TODO laukaiset eventhandler
+                    RaisePropertyChanged("Happiness");
+                   
+                }
+            }
+        }
+        public string Name { get; set; }
+        //ohjelmoidaan olio ilmoittamaan ominaisuutensa muuttumisesta
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }                       
+        }
+    }
+}
---
+++ Henkilörekisteri/Labra11Slider/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labra11Slider
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Animal elukka;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Omat alustukset
+            elukka = new Animal();
+            elukka.Name = "Musti";
+            elukka.Happiness = 0;
+            MyProgressBar.DataContext = elukka;
+            tbAnimal.DataContext = elukka;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //TO DO lisätään olion happiness ominaisuuutta
+            Animal olio = (Animal)MyProgressBar.DataContext;
+            olio.Happiness++;
+            
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            //Luodaan toinen elukka
+            Animal kissa = new Animal();
+            kissa.Name = "Misse";
+            kissa.Happiness = 50;
+            MyProgressBar.DataContext = kissa;
+            tbAnimal.DataContext = kissa;
+
+        }
+    }
+}
